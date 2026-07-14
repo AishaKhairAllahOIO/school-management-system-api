e1bf0f97d4b317efd4755a2262c19e4354e1f7df
Shahd <shahdeslim0@gmail.com>
2026-07-14 14:56:48 +0300

    refactor : add website to the basic method

--- app/Services/Setting/SchoolSettingsService.php
+++ app/Services/Setting/SchoolSettingsService.php
@@ -154,6 +154,7 @@ public function getImageById(int $id)
         $school=School::find(1);
         return ['schoolName'=>$school->school_name? :"school",
                 'shortName'=>$school->short_name? :"sch",
+                'website'=>$school->website?:null,
                 'logo'=> $school->logo_url ? (str_starts_with($school->logo_url, 'http') ? $school->logo_url : asset('storage/' . $school->logo_url)):null];
     }
 }

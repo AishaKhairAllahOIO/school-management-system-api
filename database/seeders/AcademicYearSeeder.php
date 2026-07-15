0eacd5c3cb6672587458b66d916a74cc2b2c34ff
nouralisaeh <nournour.ahmad.1284@gmail.com>
2026-07-15 17:19:45 +0300

    fix : config apdate

--- database/seeders/AcademicYearSeeder.php
+++ database/seeders/AcademicYearSeeder.php
@@ -16,7 +16,9 @@ public function run(): void
 {
     AcademicYear::updateOrCreate(
         ['id' => 1],
-        ['year_name' => '2025-2026', 'start_date' => '2025-09-01', 'end_date' => '2026-12-30']
+        ['year_name' => '2025-2026', 'start_date' => '2025-09-01', 'end_date' => '2026-12-30',
+         'is_current' => true
+        ]
     );
 }
 }

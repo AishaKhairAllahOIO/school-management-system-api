8e36089f4076adb17e0f8c65417c1ebd62eb7f99
AishaKhairAllahOIO <aishakhairallah3@gmail.com>
2026-08-09 14:13:54 +0300

    Fix Firebase credentials from base64

--- config/firebase.php
+++ config/firebase.php
@@ -50,8 +50,10 @@ return [
              *
              */
 
-            'credentials' => env('FIREBASE_CREDENTIALS', env('GOOGLE_APPLICATION_CREDENTIALS')),
-
+'credentials' => env('FIREBASE_CREDENTIALS')
+    ?: (env('FIREBASE_CREDENTIALS_BASE64')
+        ? json_decode(base64_decode(env('FIREBASE_CREDENTIALS_BASE64')), true)
+        : env('GOOGLE_APPLICATION_CREDENTIALS')),
             /*
              * ------------------------------------------------------------------------
              * Firebase Auth Component

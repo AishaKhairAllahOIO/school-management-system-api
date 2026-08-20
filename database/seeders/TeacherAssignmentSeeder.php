6fabc3422fc147f2c1904ce2be4b6ed2b289e54a
nouralisaeh <nournour.ahmad.1284@gmail.com>
2026-08-20 13:06:14 +0300

    Feat : add counselor feature to mobile

--- database/seeders/TeacherAssignmentSeeder.php
+++ database/seeders/TeacherAssignmentSeeder.php
@@ -11,31 +11,31 @@ class TeacherAssignmentSeeder extends Seeder
 {
     public function run(): void
     {
-        $mathId       = Staff::where('user_id', 3)->value('id');
-        $physicsId    = Staff::where('user_id', 21)->value('id');
-        $chemistryId  = Staff::where('user_id', 15)->value('id');
-        $arabicId     = Staff::where('user_id', 19)->value('id');
-        $islamicId    = Staff::where('user_id', 25)->value('id');
-        $englishId    = Staff::where('user_id', 22)->value('id');
-        $frenchId     = Staff::where('user_id', 16)->value('id');
-        $biologyId    = Staff::where('user_id', 13)->value('id');
-        $historyId    = Staff::where('user_id', 23)->value('id');
-        $geoId        = Staff::where('user_id', 24)->value('id');
-        $informaticsId= Staff::where('user_id', 26)->value('id');
-        $peId         = Staff::where('user_id', 17)->value('id');
-        $artsId       = Staff::where('user_id', 18)->value('id');
-        $musicId      = Staff::where('user_id', 27)->value('id');
+        $mathId = Staff::where('user_id', 3)->value('id');
+        $physicsId = Staff::where('user_id', 21)->value('id');
+        $chemistryId = Staff::where('user_id', 15)->value('id');
+        $arabicId = Staff::where('user_id', 19)->value('id');
+        $islamicId = Staff::where('user_id', 25)->value('id');
+        $englishId = Staff::where('user_id', 22)->value('id');
+        $frenchId = Staff::where('user_id', 16)->value('id');
+        $biologyId = Staff::where('user_id', 13)->value('id');
+        $historyId = Staff::where('user_id', 23)->value('id');
+        $geoId = Staff::where('user_id', 24)->value('id');
+        $informaticsId = Staff::where('user_id', 26)->value('id');
+        $peId = Staff::where('user_id', 17)->value('id');
+        $artsId = Staff::where('user_id', 18)->value('id');
+        $musicId = Staff::where('user_id', 27)->value('id');
 
         $teacherMapping = [
-            1  => [$mathId, $physicsId],    // الرياضيات (5 حصص) -> بديله الفيزياء
-            2  => [$physicsId],             // الفيزياء
-            3  => [$chemistryId],           // الكيمياء
-            4  => [$arabicId, $islamicId],  // اللغة العربية (5 حصص) -> بديله التربية الإسلامية
-            5  => [$englishId],             // اللغة الإنكليزية
-            6  => [$frenchId],              // اللغة الفرنسية
-            7  => [$biologyId],             // العلوم
-            8  => [$historyId],             // التاريخ
-            9  => [$geoId],                 // الجغرافيا
+            1 => [$mathId, $physicsId],    // الرياضيات (5 حصص) -> بديله الفيزياء
+            2 => [$physicsId],             // الفيزياء
+            3 => [$chemistryId],           // الكيمياء
+            4 => [$arabicId, $islamicId],  // اللغة العربية (5 حصص) -> بديله التربية الإسلامية
+            5 => [$englishId],             // اللغة الإنكليزية
+            6 => [$frenchId],              // اللغة الفرنسية
+            7 => [$biologyId],             // العلوم
+            8 => [$historyId],             // التاريخ
+            9 => [$geoId],                 // الجغرافيا
             10 => [$islamicId],             // التربية الإسلامية
             11 => [$informaticsId],         // المعلوماتية
             12 => [$peId],                  // الرياضة
@@ -63,7 +63,8 @@ class TeacherAssignmentSeeder extends Seeder
                     $assignedTeacherId = null;
 
                     foreach ($teacherMapping[$baseSubject] as $teacherId) {
-                        if (!$teacherId) continue;
+                        if (!$teacherId)
+                            continue;
 
                         $currentLoad = $teacherWorkload[$teacherId] ?? 0;
 
@@ -77,7 +78,8 @@ class TeacherAssignmentSeeder extends Seeder
                     if (!$assignedTeacherId) {
                         $allTeachers = [$mathId, $physicsId, $chemistryId, $arabicId, $islamicId, $englishId, $frenchId, $biologyId, $historyId, $geoId, $informaticsId, $peId, $artsId, $musicId];
                         foreach ($allTeachers as $fallbackId) {
-                            if (!$fallbackId) continue;
+                            if (!$fallbackId)
+                                continue;
                             $currentLoad = $teacherWorkload[$fallbackId] ?? 0;
                             if (($currentLoad + $periods) <= $maxWeeklyPeriods) {
                                 $assignedTeacherId = $fallbackId;
